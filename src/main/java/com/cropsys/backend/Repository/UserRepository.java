08f632708f584758e14a27fd4b6caf97898ef98a
harshil kothiya <harshilkothiya2005@gmail.com>
2026-04-24 17:14:00 +0530

    ok

--- src/main/java/com/cropsys/backend/Repository/UserRepository.java
+++ src/main/java/com/cropsys/backend/Repository/UserRepository.java
@@ -5,9 +5,10 @@ package com.cropsys.backend.Repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cropsys.backend.model.User;
-
+@Repository
 public interface UserRepository extends JpaRepository<User,Long>{
 
   
